2a1b9633e70c8cb169ccd93617d690067e094109
Bryan Hockey <bhock@umich.edu>
2016-10-31 18:23:42 -0500

    don't requrie the engine

--- lib/rails_view_adapters.rb
+++ lib/rails_view_adapters.rb
@@ -1,5 +1,4 @@
 require "rails_view_adapters/version"
-require "rails_view_adapters/engine" if defined?(Rails)
 require "rails_view_adapters/adapter_base"
 require "rails_view_adapters/map"
 require "rails_view_adapters/definition_proxy"
