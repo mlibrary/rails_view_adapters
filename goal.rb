5d18b71132941f2733e5201cb730d0485975e10d
Bryan Hockey <bhock@umich.edu>
2016-11-01 18:45:02 -0500

    remove notes file goal.rb

--- goal.rb
+++
@@ -1,9 +0,0 @@
-
-# defines a class IngestAdapter
-Adapter.define(:ingest_adapter) do
-  map_date :created_at, :created_at, Time::DATE_FORMATS[:dpn]
-  map_simple      :ingest_id, :ingest_id
-  map_belongs_to  :bag,       :bag,       sub_method: :uuid
-  map_bool        :ingested,  :ingested
-  map_has_many :nodes, :replicating_nodes, model_class: Node, sub_method: :namespace
-end
